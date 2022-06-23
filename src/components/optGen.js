1c9503361bf4679fa705c3adaa8e3f6185dd53ee
fvd24 <fvd_24_93@hotmail.com>
2022-06-22 21:22:32 -0500

    create the component optgen that generate the options of the selects

---
+++ src/components/optGen.js
@@ -0,0 +1,16 @@
+export default function OptGen({option, defSent, handleChange, handleClick}) {
+    
+    return(
+        <>
+            <select onChange={handleChange}>
+                <option value="">{defSent}</option>
+                {option.map((item) => {
+                    return(
+                        <option key={item.cvegeo} value={item.nom_agee || item.nom_agem}>{item.nom_agee || item.nom_agem}</option>
+                    )
+                })}
+            </select>
+            <button onClick={handleClick}>Buscar</button>
+        </>
+    )
+}
