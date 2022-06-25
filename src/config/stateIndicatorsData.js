7e951acc379c2d6b5a7e86af78f0bf1a9a500447
fvd24 <fvd_24_93@hotmail.com>
2022-06-25 10:05:30 -0500

    Create file that return a json with the indicators of income and outflow of money of each state

---
+++ src/config/stateIndicatorsData.js
@@ -0,0 +1,198 @@
+
+const indicatorData = [
+    {
+        cvegeo: "01",
+        indicatorIng: "440816",
+        indicatorEg: "440827",
+        name: "Aguascalientes"
+    },
+    {
+        cvegeo: "02",
+        indicatorIng: "440839",
+        indicatorEg: "440851",
+        name: "Baja California"
+    },
+    {
+        cvegeo: "03",
+        indicatorIng: "440862",
+        indicatorEg: "440874",
+        name: "Baja California Sur"
+    },
+    {
+        cvegeo: "04",
+        indicatorIng: "440886",
+        indicatorEg: "440898",
+        name: "Campeche"
+    },
+    {
+        cvegeo: "05",
+        indicatorIng: "440910",
+        indicatorEg: "440922",
+        name: "Coahuila de Zaragoza"
+    },
+    {
+        cvegeo: "06",
+        indicatorIng: "440934",
+        indicatorEg: "440945",
+        name: "Colima"
+    },
+    {
+        cvegeo: "07",
+        indicatorIng: "440956",
+        indicatorEg: "440968",
+        name: "Chiapas"
+    },
+    {
+        cvegeo: "08",
+        indicatorIng: "440980",
+        indicatorEg: "440992",
+        name: "Chihuahua"
+    },
+    {
+        cvegeo: "09",
+        indicatorIng: "441541",
+        indicatorEg: "441552",
+        name: "Ciudad de Mexico"
+    },
+    {
+        cvegeo: "10",
+        indicatorIng: "441004",
+        indicatorEg: "441016",
+        name: "Durango"
+    },
+    {
+        cvegeo: "11",
+        indicatorIng: "441027",
+        indicatorEg: "441039",
+        name: "Guanajuato"
+    },
+    {
+        cvegeo: "12",
+        indicatorIng: "441051",
+        indicatorEg: "441063",
+        name: "Guerrero"
+    },
+    {
+        cvegeo: "13",
+        indicatorIng: "441075",
+        indicatorEg: "441087",
+        name: "Hidalgo"
+    },
+    {
+        cvegeo: "14",
+        indicatorIng: "441098",
+        indicatorEg: "441110",
+        name: "Jalisco"
+    },
+    {
+        cvegeo: "15",
+        indicatorIng: "441121",
+        indicatorEg: "441132",
+        name: "Mexico"
+    },
+    {
+        cvegeo: "16",
+        indicatorIng: "441144",
+        indicatorEg: "441156",
+        name: "Michoacan de Ocampo"
+    },
+    {
+        cvegeo: "17",
+        indicatorIng: "441167",
+        indicatorEg: "441179",
+        name: "Morelos"
+    },
+    {
+        cvegeo: "18",
+        indicatorIng: "441191",
+        indicatorEg: "441202",
+        name: "Nayarit"
+    },
+    {
+        cvegeo: "19",
+        indicatorIng: "441214",
+        indicatorEg: "441225",
+        name: "Nuevo Leon"
+    },
+    {
+        cvegeo: "20",
+        indicatorIng: "441237",
+        indicatorEg: "441249",
+        name: "Oaxaca"
+    },
+    {
+        cvegeo: "21",
+        indicatorIng: "441261",
+        indicatorEg: "441272",
+        name: "Puebla"
+    },
+    {
+        cvegeo: "22",
+        indicatorIng: "441284",
+        indicatorEg: "441296",
+        name: "Queretaro"
+    },
+    {
+        cvegeo: "23",
+        indicatorIng: "441308",
+        indicatorEg: "441319",
+        name: "Quintana Roo"
+    },
+    {
+        cvegeo: "24",
+        indicatorIng: "441331",
+        indicatorEg: "441343",
+        name: "San Luis Potosi"
+    },
+    {
+        cvegeo: "25",
+        indicatorIng: "441355",
+        indicatorEg: "441366",
+        name: "Sinaloa"
+    },
+    {
+        cvegeo: "26",
+        indicatorIng: "441378",
+        indicatorEg: "441390",
+        name: "Sonora"
+    },
+    {
+        cvegeo: "27",
+        indicatorIng: "441402",
+        indicatorEg: "441413",
+        name: "Tabasco"
+    },
+    {
+        cvegeo: "28",
+        indicatorIng: "441425",
+        indicatorEg: "441436",
+        name: "Tamaulipas"
+    },
+    {
+        cvegeo: "29",
+        indicatorIng: "441447",
+        indicatorEg: "441458",
+        name: "Tlaxcala"
+    },
+    {
+        cvegeo: "30",
+        indicatorIng: "441470",
+        indicatorEg: "441482",
+        name: "Veracruz de Ignacio de la Llave"
+    },
+    {
+        cvegeo: "31",
+        indicatorIng: "441494",
+        indicatorEg: "441505",
+        name: "Yucatan"
+    },
+    {
+        cvegeo: "32",
+        indicatorIng: "441517",
+        indicatorEg: "441529",
+        name: "Zacatecas"
+    },
+
+];
+
+export default indicatorData;
